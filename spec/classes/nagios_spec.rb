aba5981a92ca393791b878938383a04f05a561eb
Luke <lkreczko@googlemail.com>
2014-01-02 15:31:12 +0100

    moving all facts into one definition

--- spec/classes/nagios_spec.rb
+++ spec/classes/nagios_spec.rb
@@ -4,11 +4,9 @@ describe 'nagios' do
 
   let(:title) { 'nagios' }
   let(:node) { 'testing.phy.bris.ac.uk' }
-  let(:facts) { { :ipaddress => '10.13.37.100' } }
-  let(:facts) { {:processorcount => 1 } }
+  let(:facts) { {:ipaddress => '10.13.37.100', :processorcount => 1, :osfamily => 'RedHat' } }
   
   describe 'Test standard installation on RedHat (client)' do
-  	let(:facts) { {:osfamily => 'RedHat' } }
     it { should contain_package('nagios-plugins').with_ensure('present') }
     it { should contain_package('nrpe').with_ensure('present') }
     it { should contain_service('nrpe').with_ensure('running') }
@@ -16,7 +14,6 @@ describe 'nagios' do
   end
   
   describe 'Test standard installation on RedHat (server)' do
-  	let(:facts) { {:osfamily => 'RedHat' } }
     let(:params) { {:is_server => true } }
     it { should contain_package('nagios').with_ensure('present') }
     it { should contain_service('nagios').with_ensure('running') }
