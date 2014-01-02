be1d22c78b7c7557cc4d5a31226fc2bc15077406
Luke <lkreczko@googlemail.com>
2014-01-02 15:17:20 +0100

    Added needed facts for tests

--- spec/classes/nagios_spec.rb
+++ spec/classes/nagios_spec.rb
@@ -5,15 +5,18 @@ describe 'nagios' do
   let(:title) { 'nagios' }
   let(:node) { 'testing.phy.bris.ac.uk' }
   let(:facts) { { :ipaddress => '10.13.37.100' } }
+  let(:facts) { {:processorcount => 1 } }
   
-  describe 'Test standard installation (client)' do
+  describe 'Test standard installation on RedHat (client)' do
+  	let(:facts) { {:osfamily => 'RedHat' } }
     it { should contain_package('nagios-plugins').with_ensure('present') }
     it { should contain_package('nrpe').with_ensure('present') }
     it { should contain_service('nrpe').with_ensure('running') }
     it { should contain_service('nrpe').with_enable('true') }
   end
   
-  describe 'Test standard installation (server)' do
+  describe 'Test standard installation on RedHat (server)' do
+  	let(:facts) { {:osfamily => 'RedHat' } }
     let(:params) { {:is_server => true } }
     it { should contain_package('nagios').with_ensure('present') }
     it { should contain_service('nagios').with_ensure('running') }
