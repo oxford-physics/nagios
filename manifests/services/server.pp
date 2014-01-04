366316f5ddf770aa7997842d6a7d45e286c27d70
Luke <lkreczko@googlemail.com>
2014-01-04 15:37:30 +0100

    Added services to server set up

--- manifests/services/server.pp
+++ manifests/services/server.pp
@@ -4,4 +4,28 @@ class nagios::services::server {
   class { 'nagios::services::client': }
 
   class { 'nagios::services::nagios': }
+
+  include nagios::commands
+  include nagios::plugins::all
+  include nagios::plugins::server
+  include nagios::templates
+  
+  # Start the Nagios service, and make it restart if there have been changes to
+  # the config
+  service { 'nagios':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => false,
+    require    => [Package['nagios'], File['nagios.cfg']],
+  }
+
+  # NSCA service to accept passive checks
+  service { 'nsca':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => [Service['nagios'], Package['nsca'], File['nsca.cfg']],
+  }
 }
