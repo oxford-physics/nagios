a884ad9421f2e9958504ffdbc697f861ad9649c5
kashif74 <kashif.alig@gmail.com>
2014-07-21 15:04:57 +0100

    fix fixes.pp for nagios_services.d

--- manifests/fixes.pp
+++ manifests/fixes.pp
@@ -4,9 +4,9 @@
 # https://tickets.puppetlabs.com/browse/PUP-1327
 class nagios::fixes {
   exec { 'fix_nagios_permissions':
-    command => 'chown -R root:nagios /etc/nagios/nagios* ; chmod -R 640 /etc/nagios/nagios*',
+    command => 'chown -R root:nagios /etc/nagios/nagios* ; chmod -R 640 /etc/nagios/nagios*.cfg ; chmod -R 640 /etc/nagios/nagios_services.d/*.cfg;',
     # test if already owned by root:nagios group
-    onlyif  => 'test $(find /etc/nagios/nagios*.cfg ! -user root -o ! -group nagios | wc -l) -gt 0',
+    onlyif  => 'test $(find /etc/nagios/nagios* ! -user root -o ! -group nagios | wc -l) -gt 0',
     path    => ['/bin', '/usr/bin'],
   }
 }
