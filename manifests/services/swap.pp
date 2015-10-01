a25f80403355f9d3b0376203fc303cf76303df10
kashif74 <kashif.alig@gmail.com>
2015-10-01 10:29:45 +0100

    change notification option

--- manifests/services/swap.pp
+++ manifests/services/swap.pp
@@ -1,16 +1,20 @@
 # Check for available swap space
-class nagios::services::swap inherits nagios::params {
+class nagios::services::swap (
+ $swap_notification = '0', 
+)inherits nagios::params 
+ {
    nagios::config::nrpe { 'check_swap':
     command => 'check_swap -w 50% -c 20%',
   }
 
   @@nagios_service { "check_swap_${::fqdn}":
-    check_command       => 'check_nrpe!check_swap',
-    host_name           => $::fqdn,
-    service_description => 'Swap',
-    use                 => '5min-service',
-    target              => "/etc/nagios/nagios_services.d/${::fqdn}.cfg",
-    tag                 => $nagios_server,
+    check_command          => 'check_nrpe!check_swap',
+    host_name              => $::fqdn,
+    service_description    => 'Swap',
+    use                    => '5min-service',
+    notifications_enabled  => '$swap_notification',
+    target                 => "/etc/nagios/nagios_services.d/${::fqdn}.cfg",
+    tag                    => $nagios_server,
   }
 
   @@nagios_servicedependency { "check_swap_${::fqdn}":
--- manifests/services/zombies.pp
+++ manifests/services/zombies.pp
@@ -1,17 +1,19 @@
 #Nagios monitoring for zombie processes
-class nagios::services::zombies inherits nagios::params {
+class nagios::services::zombies ( 
+  $zombie_notification = '0',
+) inherits nagios::params {
   
    nagios::config::nrpe { 'check_zombie_procs':
     command => 'check_procs -w 40 -c 80 -s Z',
   }
   @@nagios_service { "check_zombie_procs_${::fqdn}":
-    check_command       => 'check_nrpe!check_zombie_procs',
-    host_name           => $::fqdn,
-    service_description => 'Zombie procs',
-    use                 => '5min-service',
-    target              => "/etc/nagios/nagios_services.d/${::fqdn}.cfg",
-    notifications_enabled => false,
-    tag                 => $nagios_server,
+    check_command          => 'check_nrpe!check_zombie_procs',
+    host_name              => $::fqdn,
+    service_description    => 'Zombie procs',
+    use                    => '5min-service',
+    target                 => "/etc/nagios/nagios_services.d/${::fqdn}.cfg",
+    notifications_enabled  => $zombie_notification,
+    tag                    => $nagios_server,
   }
 
   @@nagios_servicedependency { "check_zombie_procs_${::fqdn}":
