6d81e3c9be4e2ba95a2c2eeeee26f20f9ee40f9e
kashif74 <kashif.alig@gmail.com>
2015-10-01 10:59:39 +0100

    change check_load

--- manifests/services/load.pp
+++ manifests/services/load.pp
@@ -1,18 +1,21 @@
 # Monitoring of the CPU load
-class nagios::services::load inherits nagios::params {
+class nagios::services::load (
+  $load_notification = '1',
+  $load_command      = 'check_load -r -w 1.5, 1.4 1.3 -c 1.8, 1.7, 1.6'
+) inherits nagios::params {
   
   nagios::config::nrpe { 'check_load':
-  command => 'check_load -r -w 1.6, 1.4, 1.2 -c 2, 1.8, 1.5',
+  command => $load_command,
   }
-  
   @@nagios_service { "check_load_${::fqdn}":
-    check_command       => 'check_nrpe!check_load',
-    host_name           => $::fqdn,
-    service_description => 'Load',
-    use                 => '3min-service',
-    servicegroups       => 'load',
-    target              => "/etc/nagios/nagios_services.d/${::fqdn}.cfg",
-    tag                 => $nagios_server,
+    check_command         => 'check_nrpe!check_load',
+    host_name             => $::fqdn,
+    service_description   => 'Load',
+    use                   => '3min-service',
+    servicegroups         => 'load',
+    notifications_enabled => '$load_notification',
+    target                => "/etc/nagios/nagios_services.d/${::fqdn}.cfg",
+    tag                   => $nagios_server,
   }
 
   @@nagios_servicedependency { "check_load_${::fqdn}":
