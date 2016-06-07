d21c228abd7d32bb4612fcf63841136ad2acf373
kashif74 <kashif.alig@gmail.com>
2016-06-07 09:56:09 +0100

    need ruby json for puppet check

--- manifests/services/puppet.pp
+++ manifests/services/puppet.pp
@@ -7,7 +7,9 @@ class nagios::services::puppet inherits nagios::params {
   command => 'check_puppet -w 3600 -c 7200',
   sudo => true,
 }  
-
+  #SB June 2016
+  #https://roidelapluie.be/wiki/puppet-no-such-file-to-load-json-loaderror.html
+  ensure_packages (ruby-json)
   @@nagios_service { "check_puppet_${::fqdn}":
     check_command       => 'check_nrpe!check_puppet',
     host_name           => $::fqdn,
