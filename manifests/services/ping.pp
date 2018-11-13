d1f358401aefcb938eab057d86d86d372cb0d22b
kashif74 <kashif.alig@gmail.com>
2018-11-13 11:18:24 +0000

    ping ipv6 local

--- manifests/services/ping.pp
+++ manifests/services/ping.pp
@@ -9,7 +9,12 @@ class nagios::services::ping inherits nagios::params {
     tag                 => $nagios_server,
   }
 
-  if ($::ipaddress6) {
+# Don't monitor if ipv6 address is local
+  $ipv6add = "${::ipaddress6}" 
+  $array_var1 = split($ipv6add, /::/)
+  if ($::ipaddress6) { 
+    if  ($array_var1[0] != 'fe80') {
+    notify { " ping 6 thingy worked" :}
     @@nagios_service { "check_ping_v6_${::fqdn}":
       check_command       => "check_ping_v6!${::ipaddress6}",
       host_name           => $::fqdn,
@@ -19,4 +24,5 @@ class nagios::services::ping inherits nagios::params {
       tag                 => $nagios_server,
     }
   }
+ }
 }
